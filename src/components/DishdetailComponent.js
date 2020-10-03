d8d5bf13d1103fc953461fee9da4c34f3221d48a
Aun Abbas <imaunabbas@gmail.com>
2020-10-03 12:49:13 +0500

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishetailComponent extends Component {
+class DishDetail extends Component {
   renderComments(comments) {
     if (comments != null) {
-      let options = { year: 'numeric', month: 'short', day: 'numeric' };
       return comments.map((comment) => (
         <ul key={comment.id} className='list-unstyled'>
           <li className='mb-2'>{comment.comment}</li>
           <li>
             -- {comment.author}{' '}
-            {new Date(comment.date).toLocaleDateString('en-US', options)}
+            {new Intl.DateTimeFormat('en-US', {
+              year: 'numeric',
+              month: 'short',
+              day: '2-digit',
+            }).format(new Date(Date.parse(comment.date)))}
           </li>
         </ul>
       ));
@@ -21,23 +24,25 @@ class DishetailComponent extends Component {
     const { dish } = this.props;
 
     return (
-      <div className='row'>
-        <div className='col-12 col-md-5 m-1'>
-          <Card>
-            <CardImg top src={dish.image} alt={dish.name} />
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
-        </div>
-        <div className='col-12 col-md-5 m-1'>
-          <h4>Comments</h4>
-          {this.renderComments(dish.comments)}
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12 col-md-5 m-1'>
+            <Card>
+              <CardImg top src={dish.image} alt={dish.name} />
+              <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+            </Card>
+          </div>
+          <div className='col-12 col-md-5 m-1'>
+            <h4>Comments</h4>
+            {this.renderComments(dish.comments)}
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default DishetailComponent;
+export default DishDetail;
